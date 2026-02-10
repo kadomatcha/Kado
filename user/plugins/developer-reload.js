5378ba0f1d3a12b1a6ddb41b079b9a781a1e17c5
root <root@nikusaa..>
2026-02-10 10:09:28 +0000

    auto update 1770718168003

--- user/plugins/developer-reload.js
+++ user/plugins/developer-reload.js
@@ -4,6 +4,19 @@ import { pluginManager, userManager, botInfo } from '#helper'
 
 const run = util.promisify(exec)
 
+// folder/file yang tidak mau ditampilkan atau di-commit
+const ignoreList = [
+    'auth/',
+    'node_modules/',
+    'tmp/',
+    'session/',
+    'message-store.json'
+]
+
+function shouldIgnore(filePath) {
+    return ignoreList.some(i => filePath.startsWith(i) || filePath.includes(i))
+}
+
 async function handler({ sock, m, jid }) {
     if (!userManager.trustedJids.has(m.senderId)) return
 
@@ -12,7 +25,6 @@ async function handler({ sock, m, jid }) {
         await pluginManager.loadPlugins()
         pluginManager.buildMenu()
 
-        
         const { stdout } = await run('git status --porcelain')
 
         if (!stdout.trim()) {
@@ -21,18 +33,28 @@ async function handler({ sock, m, jid }) {
             }, { quoted: m })
         }
 
-        // parsing 
-        const files = stdout
+        // parsing + filter
+        const changedLines = stdout
             .trim()
             .split('\n')
             .map(line => {
                 const status = line.slice(0, 2).trim()
                 const filePath = line.slice(3).trim()
-                return `• [${status}] ${filePath}`
+                return { status, filePath }
             })
+            .filter(f => !shouldIgnore(f.filePath))
+
+        if (!changedLines.length) {
+            return await sock.sendMessage(jid, {
+                text: 'plugin reload selesai\nperubahan hanya pada file yang di-ignore'
+            }, { quoted: m })
+        }
+
+        const files = changedLines
+            .map(f => `• [${f.status}] ${f.filePath}`)
             .join('\n')
 
-       
+        // commit hanya file yang tidak di-ignore
         await run('git add .')
         await run(`git commit -m "auto update ${Date.now()}"`)
         await run('git push')
@@ -59,8 +81,9 @@ handler.description = 'reload plugin dan push repo jika ada perubahan'
 handler.command = ['r']
 handler.category = ['developer']
 handler.meta = {
-fileName: 'developer-reload.js',
-version: '1.1',
-author: botInfo.an
+    fileName: 'developer-reload.js',
+    version: '1.2',
+    author: botInfo.an
 }
+
 export default handler
